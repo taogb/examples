52651872d5d385322a1ab42c575adac147ba0234
codehero <codehero@192.168.0.164>
2015-06-04 16:03:04 +0800

    start test

---
+++ framework/core/application.php
@@ -0,0 +1,20 @@
+<?php
+namespace core;
+
+/**
+ *
+ * @author Administrator
+ *        
+ */
+class application
+{
+    // TODO - Insert your code here
+    
+    /**
+     */
+    function __construct()
+    {
+        
+        // TODO - Insert your code here
+    }
+}
